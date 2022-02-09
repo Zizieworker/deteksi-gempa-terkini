86b692de3cdde916e9f9da1821dc85298165b77b
mochfauzi573 <mochfauzi573@gmail.com>
2022-02-10 00:19:34 +0700

    install package BeautifulSoup4

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ import gempa_terkini
 if __name__ == '__main__':
     print('Aplikasi utama')
     result = gempa_terkini.ekstraksi_data()
-    gempa_terkini.tampilkan_data(result)
+    gempa_terkini.tampilkan_data(respipult)
